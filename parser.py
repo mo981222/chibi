e088bd34c116b1496c0643467f0a7c788b2ff0c1
OI Marina <21716021@jwu.ac.jp>
2019-10-28 16:49:59 +0900

    output Expr

--- parser.py
+++ parser.py
@@ -37,7 +37,7 @@ def parse(s: str):
         s1 = s[0:pos]
         s2 = s[pos+1:]
         return Add(parse(s1), parse(s2))
-'''
+
 
 def parse(s: str):
     if s.find('+') >0:
@@ -56,3 +56,35 @@ e = parse("1*2+3")
 print(e, e.eval())
 e = parse("1+2*3")
 print(e, e.eval())
+'''
+def parse(s: str):
+    if s.find('+') >0:
+        pos = s.find('+')
+        s1 = s[0:pos]
+        s2 = s[pos+1:]
+        return Add(parse(s1), parse(s2))
+
+    if s.find('-') >0:
+        pos = s.find('-')
+        s1 = s[0:pos]
+        s2 = s[pos+1:]
+        return Sub(parse(s1), parse(s2))
+
+    if s.find('*') >0:
+        pos = s.find('*')
+        s1 = s[0:pos]
+        s2 = s[pos+1:]
+        return Mul(parse(s1), parse(s2))
+
+'''
+    if s.find('//') >0:
+        pos = s.find('//')
+        s1 = s[0:pos]
+        s2 = s[pos+1:]
+        return Div(parse(s1), parse(s2))
+    return Val(int(s))
+'''
+
+e = parse("1-2-3")
+print(e, e.eval())
+assert e.eval() == -4
