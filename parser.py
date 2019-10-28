0c515bb4b9f0d72adab162aad75d6fa4e2162b93
OI Marina <21716021@jwu.ac.jp>
2019-10-28 16:34:04 +0900

    output Add Mul

--- parser.py
+++ parser.py
@@ -1,4 +1,4 @@
-from exp import Val, Add
+from exp import Val, Add, Mul, Sub, Div
 '''
 def parse(s: str):
     num = int(s)
@@ -27,7 +27,7 @@ def parse(s: str):
         s2 = s[pos+1:]
         return Add(Val(int(s1)),Val(int(s2)))
 
-'''
+
 def parse(s: str):
     pos = s.find('+')
     if pos == -1:
@@ -37,6 +37,22 @@ def parse(s: str):
         s1 = s[0:pos]
         s2 = s[pos+1:]
         return Add(parse(s1), parse(s2))
+'''
 
-e = parse("1+2+3")
+def parse(s: str):
+    if s.find('+') >0:
+        pos = s.find('+')
+        s1 = s[0:pos]
+        s2 = s[pos+1:]
+        return Add(parse(s1), parse(s2))
+    if s.find('*') >0:
+        pos = s.find('*')
+        s1 = s[0:pos]
+        s2 = s[pos+1:]
+        return Mul(parse(s1), parse(s2))
+    return Val(int(s))
+
+e = parse("1*2+3")
+print(e, e.eval())
+e = parse("1+2*3")
 print(e, e.eval())
