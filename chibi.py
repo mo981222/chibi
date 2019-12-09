d098251ecd1d4fe0da2b344ecbe79d492de0e11d
OI Marina <21716021@jwu.ac.jp>
2019-12-09 16:51:03 +0900

    if while

--- chibi.py
+++ chibi.py
@@ -139,6 +139,13 @@ class If(Expr):
         else:
             return self.else_.eval(env)
 
+e = Block(
+    Assign('x', Val(1)),
+    Assign('y', Val(2)),
+    If(Gt(Var('x'),Var('y')),Var('x'),Var('y'))
+)
+assert e.eval == 2
+
 def conv(tree):
     if tree == 'Block':
         return conv(tree[0])
